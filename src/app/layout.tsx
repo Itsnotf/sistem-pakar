7a01082ebec2a76cc64e7ef1dae051c3f23a6a2e
itsnotf <faizaflah009@gmail.com>
2025-05-06 22:33:24 +0700

    update: make footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { Header } from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <Header />
         <div>{children}</div>
         <Toaster />
+        <Footer/>
       </body>
     </html>
   );
---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='w-full bg-gray-400/20 flex justify-center items-center py-10 text-xl font-semibold'>
+      ©Gynovia2025
+    </div>
+  )
+}
